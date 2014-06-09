a0e293373654612034cf0f66eaecb3cf67d80efb
Billy Roh <billy.roh@gmail.com>
2014-06-09 09:14:50 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Billy Roh on 6/9/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
