d8bc1eef3557966c39d50b21394725361c923acf
Billy Roh <billy.roh@gmail.com>
2014-06-09 20:48:02 -0700

    done~

--- tipcalculator/TipViewController.m
+++ tipcalculator/TipViewController.m
@@ -36,6 +36,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self updateValues];
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,10 +47,16 @@
 
 - (IBAction)onTap:(id)sender {
     [self.view endEditing:YES];
+    [self updateValues];
 }
 
-//- (void)updateValues {
-//    float billAmount = [self.billTextField.text floatValue];
-//}
+- (void)updateValues {
+    float billAmount = [self.billTextField.text floatValue];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    float totalAmount = tipAmount + billAmount;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+}
 
 @end
